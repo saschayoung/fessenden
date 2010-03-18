6a65ac5215fd5e71f95d58b6fc0218479c1fea1d
shadwe1l <shadwe1l@3175d538-b777-11de-b129-73e2b2a9d7e9>
2010-03-18 18:22:29 +0000

    update

--- geo_system_sim/reset_db.py
+++ geo_system_sim/reset_db.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import MySQLdb
 
 class reset_db:
@@ -7,30 +9,35 @@ class reset_db:
 
 
     def run(self):
-        db = MySQLdb.connect(host = "localhost",
+        db = MySQLdb.connect(host = "128.173.90.70",
                              user = "sdrc_user",
                              passwd = "sdrc_pass",
                              db = "test01")
 
-        cursor = db.cursor()
 
+        print 'table = test01_table'
         table = 'test01_table'
-        
-        
-        sql = """DROP TABLE %s""" % table
-        cursor.execute (sql)
 
-        sql = """CREATE TABLE %s(index INT AUTO_INCREMENT, rpt_pkt_num INT, 
-                 rpt_team_id INT, rpt_location CHAR(50),
-                 rpt_timestamp CHAR(50), beaon_id INT, beacon_pkt_num INT)%s""" % table
-        cursor.execute (sql)
+        print 'cursor = db.cursor()'
+        cursor = db.cursor()
+        sql1 = "DROP TABLE IF EXISTS %s" % table
+        cursor.execute (sql1)
 
 
-        sql = """CREATE INDEX index ON %s(index)""" % table
-        cursor.execute (sql)
+
+        sql2 = """CREATE TABLE %s (idx INT, rpt_pkt_num INT, 
+                                   rpt_team_id INT, rpt_location CHAR(50), 
+                                   rpt_timestamp CHAR(50), beaon_id INT, 
+                                   beacon_pkt_num INT)""" % table
+        cursor.execute (sql2)
 
 
-        cursor.close ()
+        cursor = db.cursor()
+        sql3 = """CREATE INDEX idx ON %s(idx)""" % table
+        cursor.execute (sql3)
+
+
+        cursor.close () 
         db.close ()
 
 
