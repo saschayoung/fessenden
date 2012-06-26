8a4174979c72d48e093f7d226357021ad356b604
sascha.rian.young <sascha.rian.young@gmail.com>
2012-06-26 18:16:55 +0000

    added interferer

--- systeme/interference_coordinator/interfence_coordinator.py
+++ systeme/interference_coordinator/interfence_coordinator.py
@@ -7,10 +7,10 @@ import tftpy
 
 class InterferenceCoordinator(object):
 
-    def __init__(self, remote_file='location'):
-        self.remote_file = remote_file
+    def __init__(self):
+        self.remote_file = 'location'
         self.local_file = 'local_file'
-        self.command_file = 'command_file'
+        self.command_file = '/tftp/command_file'
 
         tftp_server = '192.168.42.50'
         tftp_port = 69
@@ -33,11 +33,13 @@ class InterferenceCoordinator(object):
     def interference_logic(self):
         location = int(self.lines[0].strip('\n'))
         if location == 3:
-            s = "434000000\n"
+            s = "432000000\n"
+            s = s + "434000000\n"
             s = s + "436000000\n"
             s = s + "438000000\n"
         else:
             s = "---------\n"
+            s = s + "---------\n"
             s = s + "---------\n"
             s = s + "---------\n"
         s = s + "fin"
@@ -45,20 +47,18 @@ class InterferenceCoordinator(object):
         f.write(s)
         f.close()
 
+
     def run(self):
         while True:
             self.tftp_get()
             if self.check_file():
                 print "Received complete file."
                 self.interference_logic()
+                time.sleep(1)
+
             else:
                 print "Did not receive complete file!"
-
-            time.sleep(1)
-            
-        
-
-
+                time.sleep(0.1)
 
 
 
---
+++ systeme/interferer/interferer.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+import threading
+import time
+
+import tftpy
+import netiface
+
+from transmitter import RF
+
+
+
+class Interferer(object):
+
+    def __init__(self):
+        self.remote_file = 'command_file'
+        self.local_file = 'local_file'
+
+        tftp_server = '192.168.42.10'
+        tftp_port = 69
+        self.client = tftpy.TftpClient(tftp_server, tftp_port)
+
+        self.ip = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+
+        self.radio_command = 'off'
+
+        self.radio_transmitter = Radio()
+        
+
+    
+
+
+
+    def tftp_get(self):
+        self.client.download(self.remote_file, self.local_file)
+
+
+
+    def check_file(self):
+        f = open(self.local_file, 'rt')
+        self.lines = f.readlines()
+        f.close()
+        if self.lines[-1] != 'fin':
+            return False
+        else:
+            return True
+
+
+    def parse_file(self):
+        if self.ip == '192.168.42.61':
+            line = self.lines[0]
+        elif self.ip == '192.168.42.62':
+            line = self.lines[1]
+        elif self.ip == '192.168.42.63':
+            line = self.lines[2]
+            pass
+        elif self.ip == '192.168.42.64':
+            line = self.lines[3]
+        else:
+            print "File parsing error"
+        
+        line = line.strip('\n')
+        if line = '---------':
+            self.radio_command = 'off'
+        else:
+            self.radio_command = 'on'
+            self.radio_freq = int(line)
+
+
+
+    def run(self):
+        self.radio_transmitter.start()
+
+        while True:
+            self.tftp_get()
+            if self.check_file():
+                print "Received complete file."
+                self.parse_file()
+                if self.radio_command == 'off':
+                    continue
+                else:
+                    self.radio_transmitter.set_freq(self.radio_freq)
+                    self.radio_transmitter.set_radio_state(self.radio_command)
+
+                time.sleep(1)
+
+            else:
+                print "Did not receive complete file!"
+                time.sleep(0.1)
+            
+
+
+
+
+
+
+
+class Radio(threading.Thread):
+    """
+    Threaded radio interferer.
+
+    """
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+        self.radio_state = 'off'
+        self.rf = RF()
+
+
+    def set_radio_state(self, state):
+        """
+        Control radio operation.
+
+        This function provides a method of controlling operation of the
+        radio.
+
+        Parameters
+        ----------
+        state : str
+            One of {`on` | `off` }
+
+        """
+        self.radio_state = state
+
+
+    def set_freq(self, freq):
+        """
+        Set radio frequency.
+
+        """
+        self.freq = freq
+
+
+    def join(self, timeout=None):
+        """
+        Stop radio subsystem.
+
+        """
+        self.stop_event.set()
+        self.rf.close_gpio()
+        threading.Thread.join(self, timeout)
+
+        
+
+    def run(self):
+        """
+        Run radio interferer.
+
+        """
+        while not self.stop_event.isSet():
+            if self.radio_state == 'on':
+                try:
+                    self.rf.run(freq)
+                except Exception as e:
+                    print e
+                    time.sleep(0.01)
+                    continue
+            else:
+                time.sleep(0.01)
+            
+        
