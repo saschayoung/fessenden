62b862555d662f2b57a75ca4616c56bd5e125080
sascha.rian.young <sascha.rian.young@gmail.com>
2012-08-07 17:40:58 +0000

    add turn_180.py

---
+++ systeme/version_3/motion/turn_180.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import nxt.locator
+# from nxt.motcont import MotCont
+from nxt.motor import *
+
+
+def spin_around(b):
+    m_left = Motor(b, PORT_A)
+    m_right = Motor(b, PORT_B)
+    m_left.weak_turn(-75, 180)
+    m_right.weak_turn(75, 180)
+
+b = nxt.locator.find_one_brick()
+spin_around(b)
+
+
