197ae2cc9237e55bd95cce1de3b194f3055b4749
sascha.rian.young <sascha.rian.young@gmail.com>
2012-09-04 16:53:29 +0000

    add cw modulation

--- systeme/version_3/radio/radio_api.py
+++ systeme/version_3/radio/radio_api.py
@@ -204,7 +204,7 @@ class RadioAPI(object):
         This helper function cleans up and shuts down any open
         processes.
         """
-        self.set_op_mode('ready')
+        self.rfm22.set_op_mode('ready')
         time.sleep(0.1)
         self._kill_io()
 
